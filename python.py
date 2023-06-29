389fafa43c14112e1eaad1c15a8f6eb735834d49
TeepNooyyy <137896308+TeepNooyyy@users.noreply.github.com>
2023-06-29 20:16:44 +0300

    update

--- python.py
+++ python.py
@@ -7,21 +7,35 @@ class Boec:
         self.hp -= attack
 
 
+def check_win():
+        if piunguin.hp == 0 and sobaka.hp == 0:
+            print("ничья")
+            hfhhf = False
+        if piunguin.hp <= 0:
+            print("выиграла собака")
+            hfhhf = False
+        if sobaka.hp <= 0:
+            print("выиграла пингвина")
+            hfhhf = False
+
 hits = 0
 turn_toggler = random.randint(0,1)
-        
+hfhhf = True
 
 sobaka = Boec(2,2)
 piunguin = Boec(4,1)
-while True:
-    if piunguin.hp <= 0:
-        print("выиграла собака")
-        break
-    if sobaka.hp <= 0:
-        print("выиграла пингвина")
-        break
-    sobaka.be_attacked(piunguin.attack)
-    piunguin.be_attacked(sobaka.attack)
+while hfhhf == True:
+    check_win()
+    if turn_toggler == 1:
+        sobaka.be_attacked(piunguin.attack)
+        check_win()
+        piunguin.be_attacked(sobaka.attack)
+        check_win()
+    if turn_toggler == 0:
+        piunguin.be_attacked(sobaka.attack)
+        check_win()
+        sobaka.be_attacked(piunguin.attack)
+        check_win()
 
  
 # сделать чтобы первая аттака рандомно атаковали
